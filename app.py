15fedf653d68459f90ced9c402d52aa957c8c4e8
Desquek <azaxdom76@gmail.com>
2026-05-01 14:30:34 +0200

    Atualizar o app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session,flash
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
