fd37976b66fe771af1bed1481a7449401cab6d9c
Desquek <azaxdom76@gmail.com>
2026-07-04 21:51:03 +0200

    Atualizar o _init_.py

--- routes/_init_.py
+++ routes/_init_.py
@@ -1,1 +1,33 @@
-# clourf routes 
+ from flask import Blueprint
+
+# Importar todos os blueprints
+from .auth import auth
+from .home import home
+from .profile import profile
+from .posts import posts
+from .messages import messages
+from .favorites import favorites
+from .notifications import notifications
+from .search import search
+from .categorias import categorias_bp
+
+# Importar blueprints do admin
+from .admin.dashboard import admin_dashboard
+from .admin.usuarios import admin_usuarios
+from .admin.problemas import admin_problemas
+
+# Lista de todos os blueprints para registar no app.py
+blueprints = [
+    auth,
+    home,
+    profile,
+    posts,
+    messages,
+    favorites,
+    notifications,
+    search,
+    categorias_bp,
+    admin_dashboard,
+    admin_usuarios,
+    admin_problemas
+]
