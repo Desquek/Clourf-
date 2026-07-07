93c383001ff938cd74ac9781ad5bad30fba42b4b
Desquek <azaxdom76@gmail.com>
2026-07-07 12:40:22 +0200

    Atualizar o _init_.py

--- routes/admin/_init_.py
+++ routes/admin/_init_.py
@@ -1,1 +1,5 @@
-# pode começar a executar. 
+from .dashboard import admin_dashboard
+from .usuarios import admin_usuarios
+from .problemas import admin_problemas
+
+__all__ = ['admin_dashboard', 'admin_usuarios', 'admin_problemas']
