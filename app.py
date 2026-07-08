07f561b7d84c9141863e559473889b4da61b99f4
Desquek <azaxdom76@gmail.com>
2026-07-08 19:31:20 +0200

    Atualizar o app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from config import Config
 import os
+import database  # <-- Importante: força a execução do init_db()
 
 app = Flask(__name__)
 app.config.from_object(Config)
